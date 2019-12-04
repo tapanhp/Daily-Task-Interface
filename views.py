09bffeda7b2e8dbdc440612c290e0a4437f3d402
TruthSeekerBeast <truthseekerbeast@gmail.com>
2019-12-04 17:36:28 +0530

    print remove

--- views.py
+++ views.py
@@ -1,25 +1,23 @@
-from flask import redirect
-from flask import render_template
-from flask import request
+from flask import request,render_template,jsonify
 from app import app, db
 from models import Tasks, Project, User
 
 
 @app.route('/form', methods=["GET", "POST"])
 def create_task():
-    if request.form:
+    if request:
         try:
-            project = request.form.get('project')
-            title = request.form.get('task_title')
-            status = request.form.get('status')
-            reason = request.form.get('reason')
-            user = request.form.get('user')
-            user_id = User.query.filter_by(user_name=user)
-            project_id = Project.query.filter_by(project_name=project)
+            project = request.json.get('project', '')
+            title = request.json.get('task_title', '')
+            status = request.json.get('status', '')
+            reason = request.json.get('reason', '')
+            user = request.json.get('user', '')
+            user_id = User.query.filter_by(user_name=user).user_id
+            project_id = Project.query.filter_by(project_name=project).project_id
             task = Tasks(task_title=title, status=status, reason=reason, project_id=project_id, user_id=user_id)
             db.session.add(task)
             db.session.commit()
-            # return re
+            return jsonify({'Task': task})
         except Exception as e:
             print("couldn't store task", e)
     return render_template("login.html")
