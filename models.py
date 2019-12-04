c61ea249e43fa99207a62c54f5594886d87af50e
TruthSeekerBeast <truthseekerbeast@gmail.com>
2019-12-04 12:16:04 +0530

    Added is_admin field to user model

--- models.py
+++ models.py
@@ -13,19 +13,14 @@ class User(db.Model):
     user_id = db.Column(db.Integer, unique=True, nullable=False, primary_key=True)
     user_name = db.Column(db.String(50), unique=True, nullable=False)
     user_email = db.Column(db.String(50), unique=True, nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
     tasks = db.relationship('Tasks', backref="user")
-
-
-class TaskStatusEnum(enum.Enum):
-    green = "Green"
-    yellow = "yellow"
-    red = "red"
 
 
 class Tasks(db.Model):
     task_id = db.Column(db.Integer, unique=True, nullable=False, primary_key=True)
     task_title = db.Column(db.String(500), unique=True, nullable=False)
-    status = db.Column(db.Enum(TaskStatusEnum), nullable=False)
+    status = db.Column(db.String, nullable=False)
     reason = db.Column(db.Text, nullable=True)
     date = db.Column(db.DateTime(timezone=False), default=datetime.datetime.utcnow)
     project_id = db.Column(db.Integer, db.ForeignKey('project.project_id'), nullable=False)
