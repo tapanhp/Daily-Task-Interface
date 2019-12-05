6c06e2ab7875f27af731a6583b073f36d5f6f77a
TruthSeekerBeast <truthseekerbeast@gmail.com>
2019-12-05 18:57:33 +0530

    Added third party package marshmallow for serializing objects to save task objects

--- views.py
+++ views.py
@@ -1,48 +1,44 @@
 from flask import request, jsonify
 from models import db
-from models import Tasks, Project, User
+from models import Tasks, Project, User, TaskSchema, ProjectSchema, UserSchema
 
 
-def get_task():
-    return jsonify({'Tasks': Tasks.query.all()})
+def read_task():
+    task = Tasks.query.all()
+    task_schema = TaskSchema(many=True)
+    return jsonify(task_schema.dump(task).data)
 
 
 def create_task():
     if request:
         try:
-            # project = request.json.get('project', '')
-            title = request.json.get('task_title', '')
-            status = request.json.get('status', '')
-            reason = request.json.get('reason', '')
-            # user = request.json.get('user', '')
-            # user_id = User.query.filter_by(user_name=user).user_id
-            user_id = 1
-            # project_id = Project.query.filter_by(project_name=project).project_id
-            project_id = 1
-            task = Tasks(task_title=title, status=status, reason=reason, project_id=project_id, user_id=user_id)
+            task_schema = TaskSchema()
+            task = task_schema.load(request.json, session=db.session).data
             db.session.add(task)
             db.session.commit()
-            return jsonify({'Task': task})
+            return jsonify(task_schema.dump(task).data)
         except Exception as e:
             print("couldn't store task", e)
-        return jsonify({'Task': ''})
+            return jsonify({'Task': ''})
 
 
 def update_task(id):
-    task = Tasks.query.get(id)
-    project = request.json.get('project', task.project)
-    task.title = request.json.get('task_title', task.title)
-    task.status = request.json.get('status', task.status)
-    task.reason = request.json.get('reason', task.reason)
-    user = request.json.get('user', task.user)
-    task.user_id = User.query.filter_by(user_name=user).user_id
-    task.project_id = Project.query.filter_by(project_name=project).project_id
-    db.session.commit()
-    return jsonify({'Task': task})
+    try:
+        task = Tasks.query.get(task_id=id)
+        task.project_id = request.json.get('project', task.project_id)
+        task.title = request.json.get('task_title', task.task_title)
+        task.status = request.json.get('status', task.status)
+        task.reason = request.json.get('reason', task.reason)
+        task.user_id = request.json.get('user', task.user_id)
+        db.session.commit()
+        return jsonify({'Task': task})
+    except Exception as e:
+        print("not updated due to ", e)
+        return jsonify("Task not updated")
 
 
-def delete_task(id):
-    task = Tasks.query.get(id)
-    db.session.delete(task)
-    db.session.commit()
-    return jsonify({'result': True})
+# def delete_task(id):
+#     task = Tasks.query.get(id)
+#     db.session.delete(task)
+#     db.session.commit()
+#     return jsonify({'result': True})
