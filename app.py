10763b4d2402fb78da785b6a1faf20e2e0d76fc7
TruthSeekerBeast <truthseekerbeast@gmail.com>
2019-12-10 17:40:57 +0530

    Login required decorator Added

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import json
 import os
 import flask
 import google_auth
-from flask import render_template
+from flask import render_template, redirect
 
 app = flask.Flask(__name__)
 app.secret_key = "b']\xa0\x02\x94Rl\x15\x10z\x19\xdaEE\xbf\x08!'"
@@ -14,6 +14,16 @@ app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 
 from views import create_task, get_task, get_all_tasks, update_task, delete_task, get_all_projects, create_project, \
     delete_project, create_user, generate_report
+
+
+def login_required(func):
+    def wrapper(*args, **kwargs):
+        if google_auth.is_logged_in():
+            return func()
+        return redirect("/")
+
+    wrapper.__name__ = func.__name__
+    return wrapper
 
 
 @app.route('/')
@@ -29,45 +39,54 @@ def index():
 
 
 @app.route('/tasks/', methods=['GET'])
+@login_required
 def get_tasks():
     return get_all_tasks()
 
 
 @app.route('/task/<int:user_id>/', methods=['GET'])
+@login_required
 def get_task_main(user_id):
     return get_task(user_id)
 
 
 @app.route('/task/', methods=["POST"])
+@login_required
 def create_task_main():
     return create_task()
 
 
 @app.route('/task/<int:task_id>/', methods=['PUT'])
+@login_required
 def update_task_main(task_id):
     return update_task(task_id)
 
 
 @app.route("/task/<int:task_id>/", methods=["DELETE"])
+@login_required
 def delete_task_main(task_id):
     return delete_task(task_id)
 
 
 @app.route("/project/", methods=["GET"])
+@login_required
 def get_projects():
     return get_all_projects()
 
 
 @app.route('/project/', methods=["POST"])
+@login_required
 def create_project_main():
     return create_project()
 
 
 @app.route("/project/<int:project_id>/", methods=["DELETE"])
+@login_required
 def delete_project_main(project_id):
     return delete_project(project_id)
 
 
 @app.route("/report/", methods=["GET", "POST"])
+@login_required
 def generate_report_main():
     return generate_report()
